e5b8cb7bb8d8c7232d05f4c2eb30f83e3c579c54
Mike Harris <mharris@updox.com>
2020-12-18 11:23:46 -0500

    Created our first web server

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
