4c176f7b6adc75ca1d11d19d637d9bacd0f76194
Mike Harris <mharris@updox.com>
2020-12-18 13:35:30 -0500

    Added 404 handling

--- main.go
+++ main.go
@@ -12,5 +12,12 @@ func main() {
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my super awesome site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my super awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@goreporter.com\">support@goreporter.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "<h1>Oops! We could not find the page you were looking for.</h1>")
+	}
 }
